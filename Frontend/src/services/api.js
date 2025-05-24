f8ef50e52088700f6c6deaa9a3e88a65a7df0dad
Vansh-Vyas <160822790+VanshVA@users.noreply.github.com>
2025-05-24 16:54:31 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://melodyhub-pr26.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://melodyhub-qnq7.onrender.com';
 
 // Create axios instance
 const api = axios.create({
