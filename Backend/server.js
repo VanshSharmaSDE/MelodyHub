fd857c90da16a324d39ab976c9a85b907a036a14
Vansh-Vyas <160822790+VanshVA@users.noreply.github.com>
2025-05-24 16:41:25 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'https://melodyhub-pr26.onrender.com',
   credentials: true
 }));
 
